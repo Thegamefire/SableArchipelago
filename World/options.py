6aedb52b3d413f1a366ab4417a943b51ffb5436c
Game Fire <code@thegamefire.com>
2025-12-24 13:44:15 +0100

    Added Goal: Collect All Masks

--- World/options.py
+++ World/options.py
@@ -5,9 +5,11 @@ from Options import PerGameCommonOptions, Choice, DeathLink
 class Goal(Choice):
     """Choose the end goal.
     gliding: Choose your mask and complete the gliding.
+    all_masks: Collect every mask in the game.
     """
     display_name = "Goal"
     option_gliding = 0
+    option_all_masks = 0
     default = 0
 
 class DeathLinkMode(Choice):
