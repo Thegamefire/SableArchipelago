3f0c3bb5a86a305f40421843e0a02d64adb4b414
Game Fire <code@thegamefire.com>
2026-06-05 19:06:30 +0200

    Mod: Moved TitleSettings Patch to Separate File

--- Mod/UiHelper.cs
+++ Mod/UiHelper.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using Core.GameManagerStates;
 using GameTemplate;
-using HarmonyLib;
 using Il2CppInterop.Runtime;
 using Items;
-using MapMagic;
 using TMPro;
-using UIComponents.Layout.AtomicObjects.Buttons;
-using UIComponents.Layout.Components.Containers;
-using UIComponents.Layout.Components.Lists;
-using UIComponents.Screens;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 
 namespace com.thegamefire.sablearchipelago;
@@ -124,284 +117,170 @@ public class UiHelper
 
         return _euclidRegularFont;
     }
-    
-    [HarmonyPatch(typeof(TitleSettingsScreen), nameof(TitleScreen.OnOpen))]
-    public static class OpenTitleSettingsPatch
+    public static TMP_Text CreateText (string text, Vector2 pos, int size, Transform parent)
     {
-        private static bool _buttonAdded = false;
-
-        static void Postfix(TitleSettingsScreen __instance)
-        {
-            if (_buttonAdded) return;
-
-            var allButtons = __instance.GetComponentsInChildren<UiSelectableButton>();
-            var creditsButton = allButtons
-                .FirstOrDefault(b => b.gameObject.name.ToLower().Contains("credit"));
-            if (creditsButton == null)
-            {
-                Plugin.Log.LogError("Could not find CreditsButton field");
-                return;
-            }
-
-            var apButtonGo = Object.Instantiate(creditsButton.gameObject, 
-                creditsButton.transform.parent);
-
-            apButtonGo.transform.SetSiblingIndex(creditsButton.transform.GetSiblingIndex() + 1);
-            apButtonGo.name = "ArchipelagoButton";
-            
-            var image = apButtonGo.GetComponentsInChildren<Image>().FirstOrDefault(i => i.activeSprite!= null && i.activeSprite.name == "CreditsSettingsIcon");
-            if (image == null)
-            {
-                Plugin.Log.LogError("Couldn't find Button Icon");
-                return;
-            }
-            image.sprite = GetArchipelagoIcon();
-
-            var apButton = apButtonGo.GetComponent<UiSelectableButton>();
-            if (apButton == null)
-            {
-                Plugin.Log.LogError("Cloned GameObject has no UiSelectableButton");
-                return;
-            }
-
-            apButton.SetName("ArchipelagoButton");
-            apButton.Initialize();
-            apButton.buttonText.SetText("Archipelago");
-            
-            var uiLists = Resources.FindObjectsOfTypeAll<UiList>();
-            foreach (var uiList in uiLists)
-            {
-                if (uiList != null && uiList.name == "List")
-                {
-                    uiList.RefreshChildren();
-                    uiList.InitializeChildren();
-                    uiList.InitializeEvents();
-                }
-            }
-
-            _buttonAdded = true;
-            
-            var splitContainer = __instance.GetComponentInChildren<UiSplitInputContainer>();
-
-            var components = splitContainer.components;
-
-            var existingPanel = components[^1];
-            if (existingPanel == null)
-            {
-                Plugin.Log.LogError("[SableArchipelago] Failed To Create Custom Settings Panel");
-                return;
-            }
-
-            var apPanel = Object.Instantiate(existingPanel, existingPanel.transform.parent);
-            apPanel.name = "ArchipelagoPanel";
-            
-            
-            apPanel.transform.RemoveChildren();
-            
-            var panelUiList = apPanel.GetComponentInChildren<UiList>();
-            if (panelUiList != null)
-            {
-                panelUiList.RefreshChildren();
-                panelUiList.InitializeChildren();
-                panelUiList.InitializeEvents();
-            }
-            
-            CreateText("Hostname", new Vector2(0, 0), 24, apPanel.transform);
-            CreateInput(Plugin.ConfigApHost.Value, new Vector2(0, 0), apPanel.transform)
-                .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApHost.Value = value));
-            CreateSpacer(5, apPanel.transform);
-            
-            CreateText("Slot", new Vector2(0, 0), 24, apPanel.transform);
-            CreateInput(Plugin.ConfigApSlot.Value, new Vector2(0, 0), apPanel.transform)
-                .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApSlot.Value = value));
-            CreateSpacer(5, apPanel.transform);
-            
-            CreateText("Password", new Vector2(0, 0), 24, apPanel.transform);
-            CreateInput(Plugin.ConfigApPassword.Value, new Vector2(0, 0), apPanel.transform)
-                .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApPassword.Value = value));
-            CreateSpacer(5, apPanel.transform);
-            
-            CreateToggle("DeathLink", Plugin.ConfigApDeathlink.Value, new Vector2(0, 0),  apPanel.transform)
-                .onValueChanged.AddListener((UnityAction<bool>)
-                    (value => Plugin.ConfigApDeathlink.Value = value)
-                );
-            CreateToggle("FastTravel on Death", false, new Vector2(0, 0),  apPanel.transform);
-
-            apPanel.SetActive(false);
-
-            var newComponents = new GameObject[components.Length + 1];
-            components.CopyTo(newComponents, 0);
-            newComponents[^1] = apPanel;
-            splitContainer.components = newComponents;
-        }
-
-        static TMP_Text CreateText (string text, Vector2 pos, int size, Transform parent)
-        {
-            
-            GameObject obj = new GameObject(text, Il2CppType.Of<TextMeshProUGUI>());
-            obj.transform.SetParent(parent, false);
-            var tmp = obj.GetComponent<TextMeshProUGUI>();
-            tmp.text = text;
-            tmp.fontSize = size;
-            tmp.color = Color.black;
-            tmp.alignment = TextAlignmentOptions.Left;
-            tmp.font = GetEuclidMediumFontAsset();
-            RectTransform rect = tmp.GetComponent<RectTransform>();
-            rect.sizeDelta = new Vector2(280, 30);
-            rect.anchoredPosition = pos;
-            
-            var layout = obj.AddComponent<LayoutElement>();
-            layout.minHeight = 35;
-            return tmp;
-        }
         
-        private static TMP_InputField CreateInput(string initial, Vector2 pos, Transform parent)
-        {
-            GameObject inputObj = new GameObject("InputField", Il2CppType.Of<Image>(), Il2CppType.Of<TMP_InputField>());
-            inputObj.transform.SetParent(parent, false);
-
-            Image bg = inputObj.GetComponent<Image>();
-            bg.type = Image.Type.Sliced;
-            bg.color = new Color(0.91764f, 0.86274f, 0.84313f, 1f);
-            RectTransform rect = inputObj.GetComponent<RectTransform>();
-            rect.sizeDelta = new Vector2(200, 45);
-            rect.anchoredPosition = pos;
-
-            GameObject textArea = new GameObject("TextArea", Il2CppType.Of<RectMask2D>());
-            textArea.transform.SetParent(inputObj.transform, false);
-            RectTransform textRect = textArea.GetComponent<RectTransform>();
-            textRect.anchorMin = Vector2.zero;
-            textRect.anchorMax = Vector2.one;
-            textRect.offsetMin = new Vector2(5, 5);
-            textRect.offsetMax = new Vector2(-5, -5);
-
-            TMP_Text textComp = new GameObject("Text", Il2CppType.Of<TextMeshProUGUI>()).GetComponent<TextMeshProUGUI>();
-            textComp.transform.SetParent(textArea.transform, false);
-            textComp.text = initial;
-            textComp.fontSize = 20;
-            textComp.color = Color.black;
-            textComp.font = GetEuclidRegularFontAsset();
-            textComp.alignment = TextAlignmentOptions.Left;
-
-            RectTransform textCompRect = textComp.GetComponent<RectTransform>();
-            textCompRect.anchorMin = Vector2.zero;
-            textCompRect.anchorMax = Vector2.one;
-            textCompRect.offsetMin = Vector2.zero;
-            textCompRect.offsetMax = Vector2.zero;
-
-            TMP_InputField input = inputObj.GetComponent<TMP_InputField>();
-            input.textViewport = textRect;
-            input.textComponent = textComp;
-            input.text = initial;
-            input.caretColor = Color.black;
-
-            input.enabled = false;
-            input.enabled = true;
-            
-            var layout = inputObj.AddComponent<LayoutElement>();
-            layout.minHeight = 40;
-
-            return input;
-        }
+        GameObject obj = new GameObject(text, Il2CppType.Of<TextMeshProUGUI>());
+        obj.transform.SetParent(parent, false);
+        var tmp = obj.GetComponent<TextMeshProUGUI>();
+        tmp.text = text;
+        tmp.fontSize = size;
+        tmp.color = Color.black;
+        tmp.alignment = TextAlignmentOptions.Left;
+        tmp.font = GetEuclidMediumFontAsset();
+        RectTransform rect = tmp.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(280, 30);
+        rect.anchoredPosition = pos;
         
-        private static void CreateSpacer(float height, Transform parent)
-        {
-            var spacer = new GameObject("Spacer");
-            spacer.transform.SetParent(parent, false);
-
-            var layout = spacer.AddComponent<LayoutElement>();
-            layout.minHeight = height;
-        }
-        
-        private static Toggle CreateToggle(string label, bool initial, Vector2 pos, Transform parent)
-        {
-            GameObject toggleObj = new GameObject(
-                "Toggle",
-                Il2CppType.Of<Image>(),
-                Il2CppType.Of<Toggle>()
-            );
-
-            toggleObj.transform.SetParent(parent, false);
-
-            RectTransform rect = toggleObj.GetComponent<RectTransform>();
-            rect.sizeDelta = new Vector2(250, 40);
-            rect.anchoredPosition = pos;
-
-            Image bg = toggleObj.GetComponent<Image>();
-            bg.color = new Color(0, 0, 0, 0);
-
-            Toggle toggle = toggleObj.GetComponent<Toggle>();
-            toggle.isOn = initial;
-
-            
-            GameObject textObj = new GameObject("Label", Il2CppType.Of<TextMeshProUGUI>());
-            textObj.transform.SetParent(toggleObj.transform, false);
-
-            TMP_Text labelText = textObj.GetComponent<TextMeshProUGUI>();
-            labelText.text = label;
-            labelText.fontSize = 24;
-            labelText.color = Color.black;
-            labelText.font = GetEuclidMediumFontAsset();
-            labelText.alignment = TextAlignmentOptions.MidlineLeft;
-
-            RectTransform labelRect = labelText.GetComponent<RectTransform>();
-            labelRect.anchorMin = new Vector2(0, 0);
-            labelRect.anchorMax = new Vector2(1, 1);
-            labelRect.offsetMin = new Vector2(0, 0);
-            labelRect.offsetMax = new Vector2(-50, 0);
-
-            
-            GameObject boxObj = new GameObject("Box", Il2CppType.Of<Image>());
-            boxObj.transform.SetParent(toggleObj.transform, false);
-
-            Image box = boxObj.GetComponent<Image>();
-            box.color = new Color(0.91764f, 0.86274f, 0.84313f, 1f);
-            box.type = Image.Type.Sliced;
-
-            RectTransform boxRect = box.GetComponent<RectTransform>();
-            boxRect.anchorMin = new Vector2(1, 0.5f);
-            boxRect.anchorMax = new Vector2(1, 0.5f);
-            boxRect.pivot = new Vector2(1, 0.5f);
-            boxRect.sizeDelta = new Vector2(25, 25);
-            boxRect.anchoredPosition = new Vector2(-5, 0);
-
-            
-            GameObject checkObj = new GameObject("Check", Il2CppType.Of<Image>());
-            checkObj.transform.SetParent(boxObj.transform, false);
-
-            Image check = checkObj.GetComponent<Image>();
-            check.color = Color.black;
-
-            RectTransform checkRect = check.GetComponent<RectTransform>();
-            checkRect.anchorMin = new Vector2(0.5f, 0.5f);
-            checkRect.anchorMax = new Vector2(0.5f, 0.5f);
-            checkRect.pivot = new Vector2(0.5f, 0.5f);
-            checkRect.sizeDelta = new Vector2(12, 12);
-
-            
-            toggle.targetGraphic = box;
-            toggle.graphic = check;
-
-            checkObj.SetActive(initial);
-
-            toggle.onValueChanged.AddListener((UnityAction<bool>)(value =>
-            {
-                checkObj.SetActive(value);
-            }));
-
-            // force refresh like your input field trick
-            toggle.enabled = false;
-            toggle.enabled = true;
-
-            // optional layout element
-            var layout = toggleObj.AddComponent<LayoutElement>();
-            layout.minHeight = 40;
-
-            return toggle;
-        }
+        var layout = obj.AddComponent<LayoutElement>();
+        layout.minHeight = 35;
+        return tmp;
     }
     
+    public static TMP_InputField CreateInput(string initial, Vector2 pos, Transform parent)
+    {
+        GameObject inputObj = new GameObject("InputField", Il2CppType.Of<Image>(), Il2CppType.Of<TMP_InputField>());
+        inputObj.transform.SetParent(parent, false);
 
+        Image bg = inputObj.GetComponent<Image>();
+        bg.type = Image.Type.Sliced;
+        bg.color = new Color(0.91764f, 0.86274f, 0.84313f, 1f);
+        RectTransform rect = inputObj.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(200, 45);
+        rect.anchoredPosition = pos;
+
+        GameObject textArea = new GameObject("TextArea", Il2CppType.Of<RectMask2D>());
+        textArea.transform.SetParent(inputObj.transform, false);
+        RectTransform textRect = textArea.GetComponent<RectTransform>();
+        textRect.anchorMin = Vector2.zero;
+        textRect.anchorMax = Vector2.one;
+        textRect.offsetMin = new Vector2(5, 5);
+        textRect.offsetMax = new Vector2(-5, -5);
+
+        TMP_Text textComp = new GameObject("Text", Il2CppType.Of<TextMeshProUGUI>()).GetComponent<TextMeshProUGUI>();
+        textComp.transform.SetParent(textArea.transform, false);
+        textComp.text = initial;
+        textComp.fontSize = 20;
+        textComp.color = Color.black;
+        textComp.font = GetEuclidRegularFontAsset();
+        textComp.alignment = TextAlignmentOptions.Left;
+
+        RectTransform textCompRect = textComp.GetComponent<RectTransform>();
+        textCompRect.anchorMin = Vector2.zero;
+        textCompRect.anchorMax = Vector2.one;
+        textCompRect.offsetMin = Vector2.zero;
+        textCompRect.offsetMax = Vector2.zero;
+
+        TMP_InputField input = inputObj.GetComponent<TMP_InputField>();
+        input.textViewport = textRect;
+        input.textComponent = textComp;
+        input.text = initial;
+        input.caretColor = Color.black;
+
+        input.enabled = false;
+        input.enabled = true;
+        
+        var layout = inputObj.AddComponent<LayoutElement>();
+        layout.minHeight = 40;
+
+        return input;
+    }
+    
+    public static void CreateSpacer(float height, Transform parent)
+    {
+        var spacer = new GameObject("Spacer");
+        spacer.transform.SetParent(parent, false);
+
+        var layout = spacer.AddComponent<LayoutElement>();
+        layout.minHeight = height;
+    }
+    
+    public static Toggle CreateToggle(string label, bool initial, Vector2 pos, Transform parent)
+    {
+        GameObject toggleObj = new GameObject(
+            "Toggle",
+            Il2CppType.Of<Image>(),
+            Il2CppType.Of<Toggle>()
+        );
+
+        toggleObj.transform.SetParent(parent, false);
+
+        RectTransform rect = toggleObj.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(250, 40);
+        rect.anchoredPosition = pos;
+
+        Image bg = toggleObj.GetComponent<Image>();
+        bg.color = new Color(0, 0, 0, 0);
+
+        Toggle toggle = toggleObj.GetComponent<Toggle>();
+        toggle.isOn = initial;
+
+        
+        GameObject textObj = new GameObject("Label", Il2CppType.Of<TextMeshProUGUI>());
+        textObj.transform.SetParent(toggleObj.transform, false);
+
+        TMP_Text labelText = textObj.GetComponent<TextMeshProUGUI>();
+        labelText.text = label;
+        labelText.fontSize = 24;
+        labelText.color = Color.black;
+        labelText.font = GetEuclidMediumFontAsset();
+        labelText.alignment = TextAlignmentOptions.MidlineLeft;
+
+        RectTransform labelRect = labelText.GetComponent<RectTransform>();
+        labelRect.anchorMin = new Vector2(0, 0);
+        labelRect.anchorMax = new Vector2(1, 1);
+        labelRect.offsetMin = new Vector2(0, 0);
+        labelRect.offsetMax = new Vector2(-50, 0);
+
+        
+        GameObject boxObj = new GameObject("Box", Il2CppType.Of<Image>());
+        boxObj.transform.SetParent(toggleObj.transform, false);
+
+        Image box = boxObj.GetComponent<Image>();
+        box.color = new Color(0.91764f, 0.86274f, 0.84313f, 1f);
+        box.type = Image.Type.Sliced;
+
+        RectTransform boxRect = box.GetComponent<RectTransform>();
+        boxRect.anchorMin = new Vector2(1, 0.5f);
+        boxRect.anchorMax = new Vector2(1, 0.5f);
+        boxRect.pivot = new Vector2(1, 0.5f);
+        boxRect.sizeDelta = new Vector2(25, 25);
+        boxRect.anchoredPosition = new Vector2(-5, 0);
+
+        
+        GameObject checkObj = new GameObject("Check", Il2CppType.Of<Image>());
+        checkObj.transform.SetParent(boxObj.transform, false);
+
+        Image check = checkObj.GetComponent<Image>();
+        check.color = Color.black;
+
+        RectTransform checkRect = check.GetComponent<RectTransform>();
+        checkRect.anchorMin = new Vector2(0.5f, 0.5f);
+        checkRect.anchorMax = new Vector2(0.5f, 0.5f);
+        checkRect.pivot = new Vector2(0.5f, 0.5f);
+        checkRect.sizeDelta = new Vector2(12, 12);
+
+        
+        toggle.targetGraphic = box;
+        toggle.graphic = check;
+
+        checkObj.SetActive(initial);
+
+        toggle.onValueChanged.AddListener((UnityAction<bool>)(value =>
+        {
+            checkObj.SetActive(value);
+        }));
+
+        // force refresh like your input field trick
+        toggle.enabled = false;
+        toggle.enabled = true;
+
+        // optional layout element
+        var layout = toggleObj.AddComponent<LayoutElement>();
+        layout.minHeight = 40;
+
+        return toggle;
+    }
 }
 
 public record ApPopUp(string Title, string Description);
---
+++ Mod/UiTitleSettingsPatch.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+using HarmonyLib;
+using MapMagic;
+using UIComponents.Layout.AtomicObjects.Buttons;
+using UIComponents.Layout.Components.Containers;
+using UIComponents.Layout.Components.Lists;
+using UIComponents.Screens;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace com.thegamefire.sablearchipelago;
+
+[HarmonyPatch(typeof(TitleSettingsScreen), nameof(TitleScreen.OnOpen))]
+public static class OpenTitleSettingsPatch
+{
+    private static bool _buttonAdded = false;
+
+    static void Postfix(TitleSettingsScreen __instance)
+    {
+        if (_buttonAdded) return;
+
+        var allButtons = __instance.GetComponentsInChildren<UiSelectableButton>();
+        var creditsButton = allButtons
+            .FirstOrDefault(b => b.gameObject.name.ToLower().Contains("credit"));
+        if (creditsButton == null)
+        {
+            Plugin.Log.LogError("Could not find CreditsButton field");
+            return;
+        }
+
+        var apButtonGo = Object.Instantiate(creditsButton.gameObject,
+            creditsButton.transform.parent);
+
+        apButtonGo.transform.SetSiblingIndex(creditsButton.transform.GetSiblingIndex() + 1);
+        apButtonGo.name = "ArchipelagoButton";
+
+        var image = apButtonGo.GetComponentsInChildren<Image>().FirstOrDefault(i =>
+            i.activeSprite != null && i.activeSprite.name == "CreditsSettingsIcon");
+        if (image == null)
+        {
+            Plugin.Log.LogError("Couldn't find Button Icon");
+            return;
+        }
+
+        image.sprite = UiHelper.GetArchipelagoIcon();
+
+        var apButton = apButtonGo.GetComponent<UiSelectableButton>();
+        if (apButton == null)
+        {
+            Plugin.Log.LogError("Cloned GameObject has no UiSelectableButton");
+            return;
+        }
+
+        apButton.SetName("ArchipelagoButton");
+        apButton.Initialize();
+        apButton.buttonText.SetText("Archipelago");
+
+        var uiLists = Resources.FindObjectsOfTypeAll<UiList>();
+        foreach (var uiList in uiLists)
+        {
+            if (uiList != null && uiList.name == "List")
+            {
+                uiList.RefreshChildren();
+                uiList.InitializeChildren();
+                uiList.InitializeEvents();
+            }
+        }
+
+        _buttonAdded = true;
+
+        var splitContainer = __instance.GetComponentInChildren<UiSplitInputContainer>();
+
+        var components = splitContainer.components;
+
+        var existingPanel = components[^1];
+        if (existingPanel == null)
+        {
+            Plugin.Log.LogError("[SableArchipelago] Failed To Create Custom Settings Panel");
+            return;
+        }
+
+        var apPanel = Object.Instantiate(existingPanel, existingPanel.transform.parent);
+        apPanel.name = "ArchipelagoPanel";
+
+
+        apPanel.transform.RemoveChildren();
+
+        var panelUiList = apPanel.GetComponentInChildren<UiList>();
+        if (panelUiList != null)
+        {
+            panelUiList.RefreshChildren();
+            panelUiList.InitializeChildren();
+            panelUiList.InitializeEvents();
+        }
+
+        UiHelper.CreateText("Hostname", new Vector2(0, 0), 24, apPanel.transform);
+        UiHelper.CreateInput(Plugin.ConfigApHost.Value, new Vector2(0, 0), apPanel.transform)
+            .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApHost.Value = value));
+        UiHelper.CreateSpacer(5, apPanel.transform);
+
+        UiHelper.CreateText("Slot", new Vector2(0, 0), 24, apPanel.transform);
+        UiHelper.CreateInput(Plugin.ConfigApSlot.Value, new Vector2(0, 0), apPanel.transform)
+            .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApSlot.Value = value));
+        UiHelper.CreateSpacer(5, apPanel.transform);
+
+        UiHelper.CreateText("Password", new Vector2(0, 0), 24, apPanel.transform);
+        UiHelper.CreateInput(Plugin.ConfigApPassword.Value, new Vector2(0, 0), apPanel.transform)
+            .onEndEdit.AddListener((UnityAction<string>)(value => Plugin.ConfigApPassword.Value = value));
+        UiHelper.CreateSpacer(5, apPanel.transform);
+
+        UiHelper.CreateToggle("DeathLink", Plugin.ConfigApDeathlink.Value, new Vector2(0, 0), apPanel.transform)
+            .onValueChanged.AddListener((UnityAction<bool>)
+                (value => Plugin.ConfigApDeathlink.Value = value)
+            );
+        UiHelper.CreateToggle("FastTravel on Death", false, new Vector2(0, 0), apPanel.transform);
+
+        apPanel.SetActive(false);
+
+        var newComponents = new GameObject[components.Length + 1];
+        components.CopyTo(newComponents, 0);
+        newComponents[^1] = apPanel;
+        splitContainer.components = newComponents;
+    }
+}
