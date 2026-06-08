ca49ead183f3f95ba074e186cf5531b7e8e1b17a
Game Fire <code@thegamefire.com>
2026-06-09 00:03:45 +0200

    Mod: Now Saves LastHandledItemIndex

---
+++ Mod/Save.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+
+namespace com.thegamefire.sablearchipelago;
+
+[HarmonyPatch(typeof(SaveManager), nameof(SaveManager.SavePlayerState))]
+public class SavePatch
+{
+    public static void Postfix(SaveManager __instance)
+    {
+        int slot = __instance.LastLoadedSaveSlot;
+        var saveFiles = __instance.SaveFiles;
+        if (saveFiles == null || slot < 0 || slot >= saveFiles.Count || saveFiles[slot] == null)
+        {
+            Plugin.Log.LogError("Unable to save");
+            return;
+        }
+
+        var saveId = saveFiles[slot].ID;
+        
+        SaveUtility.Save("AP_LastHandledItemIndex", Plugin.Client.LastHandledItemIndex, saveId, false);
+        SaveUtility.Save("AP_ChumTearLocationsChecked", Plugin.Client.ChumTearLocationsChecked, saveId, false);
+    }
+}
+
+[HarmonyPatch(typeof(SaveManager), nameof(SaveManager.LoadPlayerState))]
+public class LoadPatch
+{
+    public static void Postfix(SaveManager __instance)
+    {
+        int slot = __instance.LastLoadedSaveSlot;
+        var saveFiles = __instance.SaveFiles;
+        if (saveFiles == null || slot < 0 || slot >= saveFiles.Count || saveFiles[slot] == null)
+        {
+            Plugin.Log.LogError("Unable to load save");
+            return;
+        }
+
+        var saveId = saveFiles[slot].ID;
+        
+        Plugin.Client.LastHandledItemIndex = SaveUtility.Load<int>("AP_LastHandledItemIndex", saveId);
+        Plugin.Client.ChumTearLocationsChecked = SaveUtility.Load<int>("AP_ChumTearLocationsChecked", saveId);
+    }
+}
